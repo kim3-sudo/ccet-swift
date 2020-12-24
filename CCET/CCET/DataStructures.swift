1a27c2c17402520a82e80df3ab6d89efe8659139
Sejin Kim <kim3@kenyon.edu>
2020-12-24 02:30:39 -0500

    Reattempt to commit new data structures for save data

--- CCET/CCET/DataStructures.swift
+++ CCET/CCET/DataStructures.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct Date {
+    var year: Int
+    var month: Int
+    var day: Int
+}
+
+struct Time {
+    var hour: Int
+    var minute: Int
+    var second: Int
+}
+
+struct Score {
+    var reaction: Int
+    var stmr: Int
+    var gms: Int
+}
+
+struct Result {
+    var type: String
+    var date: Date
+    var time: Time
+    var score: Score
+}
